f4cdc6b11f03f6e10e6510d01d34bdcbe7bce1c0
niuniuanran <irene.anran.niu@gmail.com>
2020-07-02 22:21:49 +1200

    Implement async and sync hashing

--- server.js
+++ server.js
@@ -11,13 +11,25 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
+const bcrypt = require("bcrypt");
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    // console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        // console.log(res); //true
+    });
+});
 
 
 //END_ASYNC
 
 //START_SYNC
+
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 
